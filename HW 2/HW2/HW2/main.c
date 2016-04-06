908e0f7814d7c222bba8319591e44eba730987d4
Aleksandr Voropaev <aleksandr.voropayev@gmail.com>
2016-04-07 01:12:47 +0300

    End of HW in 1-3 part

---
+++ HW 2/HW2/HW2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HW2
+//
+//  Created by Aleksandr Voropaev on 4/7/16.
+//  Copyright © 2016 Aleksandr Voropaev. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
