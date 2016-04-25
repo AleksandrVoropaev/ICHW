b68aa383f4a09b6a127a8bfe5de937d2a6aa3afb
Aleksandr Voropaev <aleksandr.voropayev@gmail.com>
2016-04-25 18:58:40 +0300

    Macros test don't work

---
+++ StudyPlan1/Sources/ICMacrosTest.c
@@ -0,0 +1,9 @@
+//
+//  ICMacrosTest.c
+//  StudyPlan1
+//
+//  Created by Aleksandr Voropaev on 4/25/16.
+//  Copyright © 2016 Aleksandr Voropaev. All rights reserved.
+//
+
+#include "ICMacrosTest.h"
---
+++ StudyPlan1/Sources/ICMacrosTest.h
@@ -0,0 +1,14 @@
+//
+//  ICMacrosTest.h
+//  StudyPlan1
+//
+//  Created by Aleksandr Voropaev on 4/25/16.
+//  Copyright © 2016 Aleksandr Voropaev. All rights reserved.
+//
+
+#ifndef ICMacrosTest_h
+#define ICMacrosTest_h
+
+#include <stdio.h>
+
+#endif /* ICMacrosTest_h */
